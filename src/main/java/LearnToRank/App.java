0be3ca24056ceeb7211952a25cfa925c60e5fd58
yifanshen <shensahib@gmail.com>
2013-05-22 21:54:06 -0700

    test if .idea folder still pushed?

--- src/main/java/LearnToRank/App.java
+++ src/main/java/LearnToRank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hallo Hello World!" );
+        System.out.println( "Halo Hello World!" );
     }
 }
